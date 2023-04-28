12f37bfa0f073431fbe759edc678e0c4c8e9d827
OnePuchPotato <zhibek.akylbekkyzy@alatoo.edu.kg>
2023-04-28 21:51:55 +0600

    Get template for Admin Page from themewagon (SB Admin 2)

---
+++ library/src/main/java/com/CandyShop/library/repository/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.CandyShop.library.repository;
+
+import com.CandyShop.library.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository 
+public interface ProductRepository  extends JpaRepository<Product, Long > {
+}
