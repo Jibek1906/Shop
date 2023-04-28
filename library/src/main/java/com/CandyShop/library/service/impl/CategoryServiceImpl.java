bc53952390f085bcbd371561730c9fd67a29218f
OnePuchPotato <zhibek.akylbekkyzy@alatoo.edu.kg>
2023-04-29 00:00:26 +0600

    Categories Added

--- library/src/main/java/com/CandyShop/library/service/impl/CategoryServiceImpl.java
+++ library/src/main/java/com/CandyShop/library/service/impl/CategoryServiceImpl.java
@@ -7,10 +7,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+
 @Service
 public class CategoryServiceImpl implements CategoryService {
     @Autowired
     private CategoryRepository repo;
+
     @Override
     public List<Category> findAll() {
         return repo.findAll();
